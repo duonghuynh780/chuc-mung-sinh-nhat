89ce4fadb70febe70267f625b1abc2d6bfc159b5
duonghuynh780 <151656525+duonghuynh780@users.noreply.github.com>
2025-03-18 22:15:39 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function checkValid() {
     var input = document.getElementById("nhapngaysinh");
-    if (input.value == "21/11/2003") {
+    if (input.value == "19/03/2005") {
         showImage('./assets/texts/tunganh.txt');
     } else if (input.value == "26/11/2003") {
         showImage("assets/texts/daothanhdung.txt");
